6f671aa46f5d429d5f889f80a6290233814d294b
Arpan Abhishek <arpanforbusiness@gmail.com>
2023-11-14 18:50:49 +0530

    updated APIs and build versions for newer devices

--- TEEChecker/app/src/main/java/com/ifihada/teechecker/PlatformSectionFragment.java
+++ TEEChecker/app/src/main/java/com/ifihada/teechecker/PlatformSectionFragment.java
@@ -4,12 +4,20 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.security.IKeystoreService;
 import android.security.KeyChain;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyInfo;
+import android.security.keystore.KeyProperties;
 import android.util.Log;
 
 import java.lang.reflect.Method;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +41,7 @@ public class PlatformSectionFragment extends CheckerFragment
 
     result("keystore-supported", hasKeystoreApi(), "");
     result("keystore-keytypes", getUsableTypes());
-    result("keystore-hw", hasKeystoreHardware(), "");
+//    result("keystore-hw", hasKeystoreHardware(), "");
     result("keystore-hw-keytypes", getHardwareTypes());
   }
 
@@ -54,6 +62,8 @@ public class PlatformSectionFragment extends CheckerFragment
   }
 
   private static final String[] knownKeyTypes = new String[] { "RSA", "DSA", "EC", "ECDSA", "ECDH", "AES", "DES", "DES3" };
+  private static final String KEY_ALIAS = "myKeyAlias";
+
 
   private String join(List<String> strs)
   {
@@ -106,68 +116,86 @@ public class PlatformSectionFragment extends CheckerFragment
 
   private boolean hasKeystoreApi()
   {
-    IKeystoreService svc = getKeystore();
-
-    if (svc == null)
-      return false;
-
-    try
-    {
-      return svc.test() != 0;
-    } catch (RemoteException e)
-    {
-      Log.wtf(TAG, "test() failed", e);
-    } catch (NoSuchMethodError e)
-    {
-      Log.wtf(TAG, "test() does not exist", e);
+    try {
+      boolean hasKeyStore = generateKeyAndCheckSecurityLevel();
+      removeKeyFromKeystore();
+      return hasKeyStore;
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
     return false;
   }
 
   private boolean isKeyTypeHardwareBacked(String kt)
   {
-    IKeystoreService svc = getKeystore();
-
-    if (svc == null)
-      return false;
-
-    try
-    {
-      return svc.is_hardware_backed(kt) == NO_ERROR;
-    } catch (RemoteException e)
-    {
-      Log.wtf(TAG, "is_hardware_backed(" + kt + ") failed", e);
-    } catch (NoSuchMethodError e)
-    {
-      Log.wtf(TAG, "is_hardware_backed(" + kt + ") does not exist", e);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+      return KeyChain.isBoundKeyAlgorithm(kt);
     }
-
     return false;
   }
 
   private boolean hasKeystoreHardware()
   {
-    IKeystoreService svc = getKeystore();
-
-    if (svc == null)
-      return false;
-
-    try
-    {
-      return svc.is_hardware_backed() == NO_ERROR;
-    } catch (RemoteException e)
-    {
-      Log.wtf(TAG, "is_hardware_backed() failed", e);
-    } catch (NoSuchMethodError e)
-    {
-      Log.wtf(TAG, "is_hardware_backed() does not exist", e);
-    }
-
     for (String kt : knownKeyTypes)
       if (isKeyTypeHardwareBacked(kt))
         return true;
 
     return false;
   }
+
+  private static boolean generateKeyAndCheckSecurityLevel() throws Exception {
+    KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+    keyStore.load(null);
+
+    if (!keyStore.containsAlias(KEY_ALIAS)) {
+      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(
+              KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        keyPairGenerator.initialize(new KeyGenParameterSpec.Builder(
+                KEY_ALIAS,
+                KeyProperties.PURPOSE_SIGN)
+                .setDigests(KeyProperties.DIGEST_SHA256)
+                .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+                .build());
+      }
+
+      KeyPair keyPair = keyPairGenerator.generateKeyPair();
+      PrivateKey key = keyPair.getPrivate();
+      KeyFactory factory = KeyFactory.getInstance(key.getAlgorithm(), "AndroidKeyStore");
+      KeyInfo keyInfo;
+      try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+          keyInfo = factory.getKeySpec(key, KeyInfo.class);
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            System.out.println("SECURITY LEVEL: " + keyInfo.getSecurityLevel());
+            switch(keyInfo.getSecurityLevel()) {
+              case KeyProperties.SECURITY_LEVEL_STRONGBOX:
+              case KeyProperties.SECURITY_LEVEL_TRUSTED_ENVIRONMENT: return true;
+              default: return false;
+            }
+          } else {
+            return keyInfo.isInsideSecureHardware();
+          }
+        }
+      } catch (InvalidKeySpecException e) {
+        // Not an Android KeyStore key.
+      }
+      System.out.println("Key generated successfully with alias: " + KEY_ALIAS);
+    } else {
+      System.out.println("Key with alias " + KEY_ALIAS + " already exists.");
+    }
+    return false;
+  }
+
+  private static void removeKeyFromKeystore() throws Exception {
+    KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+    keyStore.load(null);
+
+    // Remove the key entry using the alias
+    keyStore.deleteEntry(KEY_ALIAS);
+
+    System.out.println("Key with alias " + KEY_ALIAS + " deleted from keystore.");
+  }
 }
+
